beb323e98d3d7cbe6a39ada3987768c075240f55
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-06-12 19:43:14 +0300

    task 4

--- src/server.js
+++ src/server.js
@@ -5,29 +5,31 @@ import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3001'));
 
 export const setupServer = () => {
     const app = express();
+
     app.use(express.json());
+
     app.use(cors());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
-    
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    })
+        ,);
     
     app.use(router);
     
     app.use('*', notFoundHandler);
     
     app.use(errorHandler);
+
+    app.use(cookieParser());
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,50 @@
 import bcrypt from 'bcrypt';
 import { User } from '../db/models/user.js';
 import createHttpError from 'http-errors';
+import { Session } from '../db/models/session.js';
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
+import { randomBytes } from 'crypto';
 
 export const registerUser = async (payload) => {
     const encryptedPassword = await bcrypt.hash(payload.password, 10);
     const user = await User.findOne({ email: payload.email });
-
-  if (user) {
+    if (user) {
       throw createHttpError(409, 'Email in use');
     }
     
-  return await User.create({
-    ...payload,
-    password: encryptedPassword,
+    return await User.create({
+        ...payload,
+        password: encryptedPassword,
   });
 };
+
+export const loginUser = async (payload) => {
+    const user = await User.findOne({ email: payload.email });
+    if (!user) {
+        throw createHttpError(401, 'User not found');
+    }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+    }
+    await Session.deleteOne({ userId: user._id });
+
+    
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await Session.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
+  });
+};
+
+
+
+
+
